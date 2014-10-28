211ee8d4cb19ce445f078dab3afc313461487105
Dmitry Kontsevoy <dmitry.kontsevoj@gmail.com>
2014-10-28 21:13:41 +0300

    added timestamps to backups

--- db_backup/recipes/default.rb
+++ db_backup/recipes/default.rb
@@ -1,5 +1,5 @@
 package 'awscli' do
-  action :install
+	action :install
 end
 
 package 'postgresql-client' do
@@ -7,25 +7,26 @@ package 'postgresql-client' do
 end
 
 node[:deploy].each do |app_name, deploy|
-	dump_file = "/tmp/#{app_name}.sql.dump"
-	s3_dir = node[:s3_backups_path];
-	s3_path = "#{s3_dir}/#{app_name}.pg_dump"
-	script_path = "#{deploy[:deploy_to]}/shared/scripts/db_backup.sh"
+	settings = node[:db_backup]
+
+	s3_dir = settings[:s3_path];
+	script_path = File.join(deploy[:deploy_to], "/shared/scripts/db_backup.sh")
+	timestamp = Time.now.strftime("%Y.%m.%d.%H.%M.%S")
 
 	template script_path do
 	    source "db_backup.sh.erb"
 	    mode 0755
 	    group deploy[:group]
 	    variables(
-	    	access_key_id: node[:s3_access_key_id],
-			secret_access_key: node[:s3_secret_access_key],
+	    	access_key_id: settings[:s3_access_key_id],
+			secret_access_key: settings[:s3_secret_access_key],
 	    	db_host: deploy[:database][:host],
 	    	db_port: deploy[:database][:port],
 	    	db_name: deploy[:database][:database],
 	    	db_username: deploy[:database][:username],
 	    	db_password: deploy[:database][:password],
-	    	s3_path: "#{s3_dir}/#{app_name}.pg_dump",
-	    	dump_file: "/tmp/#{app_name}.sql.dump"
+	    	s3_path: File.join(s3_dir, app_name, "#{timestamp}.pg_dump")
+	    	dump_file: "/tmp/#{app_name}.psql.dump"
 	    )
 	end 
 
